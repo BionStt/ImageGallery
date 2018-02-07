edea5bb2e6f6848331e2d8de6147cf4d6f996530
Asif Rahman <asif0531@live.com>
2018-02-07 22:49:23 +0600

    product, image, mapping model added

---
+++ ImageGallery/ImageGallery.Core/Model/ProductImageMappingModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageGallery.Core.Model
+{
+    public class ProductImageMappingModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public Guid ImageId { get; set; }
+        public int SortOrder { get; set; }
+        public int Position { get; set; }
+
+        public virtual Product Product { get; set; }
+        public virtual Image Image { get; set; }
+    }
+}
