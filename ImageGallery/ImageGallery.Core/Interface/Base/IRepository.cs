83c3f1bb1560fae6c37bf242ac472dff925fc19d
Asif Rahman <asif0531@live.com>
2018-02-07 23:32:55 +0600

    repository pattern implemented

---
+++ ImageGallery/ImageGallery.Core/Interface/Base/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ImageGallery.Core.Interface.Base
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> All();
+        T Find(Guid id);
+        T FindByExpression(Expression<Func<T, bool>> predicate);
+        IQueryable<T> FindMany(Expression<Func<T, bool>> predicate);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void SaveChanges();
+
+    }
+}
---
+++ ImageGallery/ImageGalley.Data/Repository/Base/Repository.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using ImageGallery.Core.Interface.Base;
+using ImageGalley.Data.AppDbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImageGalley.Data.Repository.Base
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private DbSet<T> _entities;
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _entities = context.Set<T>();
+        }
+        public IQueryable<T> All()
+        {
+            return _entities.AsNoTracking();
+        }
+
+        public T Find(Guid id)
+        {
+            //return _entities.SingleOrDefault( == id);
+            throw new NotImplementedException();
+        }
+
+        public T FindByExpression(Expression<Func<T, bool>> predicate)
+        {
+            return _entities
+                .AsNoTracking()
+                .SingleOrDefault(predicate);
+        }
+
+        public IQueryable<T> FindMany(Expression<Func<T, bool>> predicate)
+        {
+            return _entities
+                .AsNoTracking()
+                .Where(predicate);
+        }
+
+        public void Insert(T entity)
+        {
+            try
+            {
+                if (entity == null)
+                    throw new ArgumentNullException("entity");
+
+                _entities.Add(entity);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            try
+            {
+                if (entity == null)
+                    throw new ArgumentNullException("entity");
+
+                _context.Entry(entity).State = EntityState.Modified;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public void Delete(T entity)
+        {
+            try
+            {
+                if (entity == null)
+                    throw new ArgumentNullException("entity");
+
+                _entities.Remove(entity);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
