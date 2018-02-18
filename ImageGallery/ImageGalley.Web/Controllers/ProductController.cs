981071e7b32fe5c71aaf7932721915e6266b1055
Asif Rahman <asif0531@live.com>
2018-02-18 23:55:59 +0600

    image mapping implemented, create on process

--- ImageGallery/ImageGalley.Web/Controllers/ProductController.cs
+++ ImageGallery/ImageGalley.Web/Controllers/ProductController.cs
@@ -52,6 +52,40 @@ namespace ImageGalley.Web.Controllers
             return View(new ProductCreateOrUpdateModel());
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ProductCreateOrUpdateModel model, bool continueEditing)
+        {
+            var hasError = false;
+
+            if (ModelState.IsValid)
+            {
+              
+               
+                    // generate new id for model
+                    model.Id = Guid.NewGuid();
+
+                    // map view model to entity
+                    var productEntity = _mapper.Map<ProductCreateOrUpdateModel, ProductModel>(model);
+                    productEntity.DateAdded = DateTime.Now;
+                    productEntity.DateModified = DateTime.Now;
+
+                    // save to database
+                    _productService.InsertProduct(productEntity);
+                    
+                    SaveImageMappings(model);
+                   
+
+                    if (continueEditing)
+                        return RedirectToAction("Edit", new { id = productEntity.Id, ActiveTab = model.ActiveTab });
+
+                    return RedirectToAction("Index");
+            }
+
+            // something went wrong, redisplay form
+            return View(model);
+        }
+
         // POST: /Product/Delete
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -64,5 +98,35 @@ namespace ImageGalley.Web.Controllers
             return RedirectToAction("Index");
         }
 
-    }
+
+        private void SaveImageMappings(ProductCreateOrUpdateModel model)
+        {
+            var imageMappings = new List<ProductImageMappingModel>();
+            if (model.ImageIds != null)
+            {
+                for (int i = 0; i < model.ImageIds.Count; i++)
+                {
+                    // convert sort order to int
+                    int sortOrder = Convert.ToInt32(Math.Floor(Convert.ToDouble(model.ImageSortOrder[i])));
+
+                    // check if image exist
+                    Guid imageId;
+                    if (Guid.TryParse(model.ImageIds[i], out imageId))
+                    {
+                        // create mapping entity
+                        var imageMapping = new ProductImageMappingModel
+                        {
+                            Id = Guid.NewGuid(),
+                            ProductId = model.Id,
+                            ImageId = Guid.Parse(model.ImageIds[i]),
+                            SortOrder = sortOrder,
+                            Position = i
+                        };
+
+                        imageMappings.Add(imageMapping);
+                    }
+                }
+            }
+
+        }
 }
